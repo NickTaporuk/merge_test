7a7253c25b321eaa0b637ca820cd558fe86b242c
NickTaporuk <nictaporuk@yandex.ru>
2015-07-09 21:42:10 +0300

    fix

--- merge_branc_master.sh
+++ merge_branc_master.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
+#1 проверить ветку
+#2 git pull commit push
+#3 перейти в мастер
+#4 git pull
+#
+#
 
+#####################################################################
 #
-# Deploying application to frontend and backend servers
 #
+#
+#####################################################################
 
 PROGNAME=`basename $0`
 VERSION="Version 0.1"
 AUTHOR="2015, Nickolay Kuropatkin (email: nictaporuk@yandex.ru)"
 
 
-
-
-
-
+# its work git merge --squash -s subtree --no-commit name_branch
 #echo $PROGNAME
 UNAME=$(git config --get user.name)
 UEMAIL=$(git config --get user.email)
 PARAMS="$@ --user.name \"$UNAME\" --user.email $UEMAIL"
 BRANCH=$(git branch)
 #echo "BRANCH:: $BRANCH"
-cat $BRANCH | grep 'test'
+echo $BRANCH | grep -n --color 'test'
 #git commit -am'autocommit'
 #
 #git checkout master
