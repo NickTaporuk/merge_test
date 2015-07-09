2254b89b2db3a9d69067cc57d7e506d68dbdc802
NickTaporuk <nictaporuk@yandex.ru>
2015-07-09 21:07:59 +0300

    fix

--- merge_branc_master.sh
+++ merge_branc_master.sh
@@ -8,14 +8,20 @@ PROGNAME=`basename $0`
 VERSION="Version 0.1"
 AUTHOR="2015, Nickolay Kuropatkin (email: nictaporuk@yandex.ru)"
 
-echo $PROGNAME;
-git commit -am'autocommit'
 
+
+
+
+
+#echo $PROGNAME
+UNAME=$(git config --get user.name)
+UEMAIL=$(git config --get user.email)
+PARAMS="$@ --user.name \"$UNAME\" --user.email $UEMAIL"
+BRANCH=$(git branch)
+#echo "BRANCH:: $BRANCH"
+cat $BRANCH | grep 'test'
+#git commit -am'autocommit'
 #
+#git checkout master
 #
-#
-#
-#
-git checkout master
-#
-git merge --squash -s subtree --no-commit develop
+#git merge --squash -s subtree --no-commit develop
