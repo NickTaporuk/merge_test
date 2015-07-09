1f11c1545c462b5ec7f47f6acbe6225e7fc69138
Nickolay Kuropatkin <nictaporuk@yandex.ru>
2015-07-09 17:29:36 +0300

    add git merge to master v1.0

--- merge_branc_master.sh
+++ merge_branc_master.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-branch=$(git branch);
-echo "$branch";
+#
+# Deploying application to frontend and backend servers
+#
+
+PROGNAME=`basename $0`
+VERSION="Version 0.12,"
+AUTHOR="2013, Nickolay Kuropatkin (email: nictaporuk@yandex.ru)"
+#
+git checkout master
+#
+git merge --squash -s subtree --no-commit develop
