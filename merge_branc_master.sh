249fe03a5d6445fd75c42a72742fc9e94ec73640
NickTaporuk <nictaporuk@yandex.ru>
2015-07-09 22:35:33 +0300

    fix

--- merge_branc_master.sh
+++ merge_branc_master.sh
@@ -19,12 +19,12 @@ AUTHOR="2015, Nickolay Kuropatkin (email: nictaporuk@yandex.ru)"
 
 # its work git merge --squash -s subtree --no-commit name_branch
 #echo $PROGNAME
-UNAME=$(git config --get user.name)
-UEMAIL=$(git config --get user.email)
-PARAMS="$@ --user.name \"$UNAME\" --user.email $UEMAIL"
+#UNAME=$(git config --get user.name)
+#UEMAIL=$(git config --get user.email)
+#PARAMS="$@ --user.name \"$UNAME\" --user.email $UEMAIL"
 BRANCH=$(git branch)
-#echo "BRANCH:: $BRANCH"
-echo $BRANCH | grep -n --color 'test'
+NAMEBRANCH=$(echo "$BRANCH" | grep -i '\*' | sed '{s/\*// ; /^$/d}')
+echo "$NAMEBRANCH"
 #git commit -am'autocommit'
 #
 #git checkout master
